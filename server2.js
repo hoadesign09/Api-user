884acc2abc474e0a9f0c0aee386d4bd4344bb7ce
hoadesign.09@gmail.com <hoadesign.09@gmail.com>
2023-05-11 13:44:34 +0700

    commit validate

--- server2.js
+++ server2.js
@@ -12,11 +12,22 @@ app.use(bodyParser.json());
 // connect to database
 mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true });
 
+const validateUserInput = (req, res, next) => {
+  const { fullname, phone, services } = req.body;
+
+  if (!fullname || !phone || !services) {
+    return res.status(400).json({ error: 'Vui lòng cung cấp đầy đủ thông tin người dùng.' });
+  }
+
+  // Nếu các trường bắt buộc đã được cung cấp, cho phép tiếp tục xử lý
+  next();
+}
 // create user schema
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
-  fullname: [String],
-  phone: [String],
+  fullname: {type: String, required: true},
+  phone: {type: String, required: true},
+  services: {type: String, required: true}, 
   savedShows: [String],
 });
 
@@ -34,9 +45,9 @@ app.get('/users', async (req, res) => {
   });
   
 // create new user
-app.post('/users', async (req, res) => {
-  const { email, savedShows } = req.body;
-  const user = new User({ email, savedShows });
+app.post('/users',validateUserInput,async (req, res) => {
+  const { email, savedShows, fullname, phone, services } = req.body;
+  const user = new User({ email, savedShows, fullname, phone, services });
 
   try {
     await user.save();
